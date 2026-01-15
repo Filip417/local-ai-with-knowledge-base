38091a653e97dff772abc67c6ce6fa9daaaf4510
Filip417 <sawinskif@gmail.com>
2026-01-15 23:14:37 +0100

    Many styles to select and delete

--- frontend/src/app/components/sources-list/sources-list.ts
+++ frontend/src/app/components/sources-list/sources-list.ts
@@ -1,13 +1,49 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sources-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './sources-list.html',
   styleUrls: ['./sources-list.css'],
 })
 export class SourcesList {
   @Input() sources: string[] = [];
+  @Output() deleteSource = new EventEmitter<string>();
+  
+  selectedSources = new Set<string>();
+  selectAll = false;
+
+  toggleSelectAll() {
+    if (this.selectAll) {
+      this.sources.forEach(source => this.selectedSources.add(source));
+    } else {
+      this.selectedSources.clear();
+    }
+  }
+
+  toggleSource(source: string) {
+    if (this.selectedSources.has(source)) {
+      this.selectedSources.delete(source);
+    } else {
+      this.selectedSources.add(source);
+    }
+    this.updateSelectAllState();
+  }
+
+  isSourceSelected(source: string): boolean {
+    return this.selectedSources.has(source);
+  }
+
+  updateSelectAllState() {
+    this.selectAll = this.sources.length > 0 && this.selectedSources.size === this.sources.length;
+  }
+
+  deleteSelectedSource(source: string) {
+    this.deleteSource.emit(source);
+    this.selectedSources.delete(source);
+    this.updateSelectAllState();
+  }
 }
