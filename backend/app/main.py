d244015cbfca6a187867998940e1ce7594f70e62
Filip417 <sawinskif@gmail.com>
2026-01-17 15:26:10 +0100

    Cleaner main

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,17 +2,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.chat import router as chat_router
 from app.api.file import router as vector_db_router
-from contextlib import asynccontextmanager
+import uvicorn
 
-# Optional startup/shutdown events
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # start up events
-    yield
-    # shutdown events
-    pass
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 # CORS allow Angular (localhost:4200) to talk to FastAPI
 app.add_middleware(
@@ -26,6 +18,4 @@ app.add_middleware(
 app.include_router(chat_router, prefix="/api/v1", tags=["chat"])
 app.include_router(vector_db_router, prefix="/api/v1", tags=["vector_db"])
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+uvicorn.run(app, host="0.0.0.0", port=8000)
