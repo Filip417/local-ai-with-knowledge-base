776c649b2169f23cc56baa3dec7cbdff6e71db09
Filip417 <sawinskif@gmail.com>
2026-01-17 15:32:40 +0100

    chroma client moved to main

--- backend/app/database.py
+++
@@ -1,5 +0,0 @@
-import chromadb
-from app.core.config import VECTOR_DB_COLLECTION_NAME
-
-chroma_client = chromadb.Client()
-chroma_client.create_collection(name=VECTOR_DB_COLLECTION_NAME)
--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api.chat import router as chat_router
 from app.api.file import router as file_router
 import uvicorn
+import chromadb
+from app.core.config import VECTOR_DB_COLLECTION_NAME
+
 
 app = FastAPI()
 
@@ -17,5 +20,8 @@ app.add_middleware(
 # Mount routers
 app.include_router(chat_router, prefix="/api/v1", tags=["chat"])
 app.include_router(file_router, prefix="/api/v1", tags=["file"])
+
+chroma_client = chromadb.Client()
+chroma_client.create_collection(name=VECTOR_DB_COLLECTION_NAME)
 
 uvicorn.run(app, host="0.0.0.0", port=8000)
