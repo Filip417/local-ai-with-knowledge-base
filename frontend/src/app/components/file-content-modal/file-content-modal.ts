dff0bd69d1fe32e9303ff572466c28d52f4e14bb
Filip417 <sawinskif@gmail.com>
2026-01-17 18:38:50 +0100

    modal fixed

--- frontend/src/app/components/file-content-modal/file-content-modal.ts
+++ frontend/src/app/components/file-content-modal/file-content-modal.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges, inject } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnChanges, SimpleChanges, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FileService } from '../../services/file.service';
 
@@ -9,9 +9,7 @@ import { FileService } from '../../services/file.service';
   templateUrl: './file-content-modal.html',
   styleUrls: ['./file-content-modal.css']
 })
-export class FileContentModal implements OnChanges {
-  private fileService = inject(FileService);
-  
+export class FileContentModal implements OnInit, OnChanges {
   @Input() isOpen = false;
   @Input() filename: string = '';
   @Output() close = new EventEmitter<void>();
@@ -19,6 +17,17 @@ export class FileContentModal implements OnChanges {
   content: string = '';
   isLoading = false;
   error: string | null = null;
+
+  constructor(
+    private fileService: FileService,
+    private cdr: ChangeDetectorRef
+  ) { }
+
+  ngOnInit() {
+    if (this.isOpen && this.filename) {
+      this.loadFileContent();
+    }
+  }
 
   ngOnChanges(changes: SimpleChanges) {
     if ((changes['isOpen'] || changes['filename']) && this.isOpen && this.filename) {
@@ -30,15 +39,18 @@ export class FileContentModal implements OnChanges {
     this.content = '';
     this.isLoading = true;
     this.error = null;
+    this.cdr.detectChanges();
 
     this.fileService.getFileContent(this.filename).subscribe({
       next: (content) => {
         this.content = content;
         this.isLoading = false;
+        this.cdr.detectChanges();
       },
       error: (err) => {
         this.error = `Failed to load file: ${err.message}`;
         this.isLoading = false;
+        this.cdr.detectChanges();
       }
     });
   }
