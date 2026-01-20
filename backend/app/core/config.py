3cb643c280dae697bbefc1da7e1c3345745e6b9e
Filip417 <sawinskif@gmail.com>
2026-01-20 21:43:07 +0100

    New better prompt

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,7 +2,14 @@ VECTOR_DB_COLLECTION_NAME = "my_collection"
 CONTEXT_LIMIT = 131072
 MAX_OUTPUT_TOKENS = 31000
 MODEL_ABSOLUTE_PATH = r"C:\Users\sawin\coding-projects\local-ai-with-knowledge-base\local-ai-with-knowledge-base\backend\app\llmmodels\Llama-3.2-3B-Instruct-Q5_K_M.gguf"
-ROLE_LLM_PROMPT="You are a helpful assistant."
+ROLE_LLM_PROMPT="""
+You are an AI personal assistant.\n
+You have access to personal knowledge base.\n
+Ground all responses strictly in the provided context.\n
+Synthesize insights across documents, identify key themes.\n
+If the context lacks information, explicitly state so.\n
+Prioritize accuracy, structural clarity and direct response over conversational filler.
+"""
 
 # GPU Configuration
 # Set N_GPU_LAYERS to the number of layers to offload to GPU
