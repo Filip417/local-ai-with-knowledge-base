00a9efdfd42eb41f382277a94463c2610e2ef7b4
Filip417 <sawinskif@gmail.com>
2026-01-20 19:17:59 +0100

    Updated front supported format

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This repository features a privacy-first, Retrieval-Augmented Generation (RAG) p
 1. backend - uvicorn
 from repo root\backend
 ```
+$env:CUDA_PATH = "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v13.0"
 python -m app.main
 ```
 
--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,6 @@
 VECTOR_DB_COLLECTION_NAME = "my_collection"
-CONTEXT_LIMIT = 10000
-MAX_OUTPUT_TOKENS = 4000
+CONTEXT_LIMIT = 131072
+MAX_OUTPUT_TOKENS = 31000
 MODEL_ABSOLUTE_PATH = r"C:\Users\sawin\coding-projects\local-ai-with-knowledge-base\local-ai-with-knowledge-base\backend\app\llmmodels\Llama-3.2-3B-Instruct-Q5_K_M.gguf"
 ROLE_LLM_PROMPT="You are a helpful assistant."
 
